b02483e60715c80f4a8b672134ff5014c6f1b5c3
uoboda-splunk <uoboda@splunk.com>
2022-01-30 20:39:58 +0100

    more commands [run-it]

--- integration_tests/automatic_setup.sh
+++ integration_tests/automatic_setup.sh
@@ -61,25 +61,25 @@ sudo docker build -t snmp-local .
 
 sudo docker save snmp-local > myimage.tar
 sudo microk8s ctr image import myimage.tar
-#
-#sudo docker pull splunk/splunk:latest
-#echo $(green "Running Splunk in Docker")
-#sudo docker run -d -p 8000:8000 -p 8088:8088 -p 8089:8089 -e SPLUNK_START_ARGS='--accept-license' -e SPLUNK_PASSWORD='changeme2' splunk/splunk:latest
-#
-#wait_for_splunk
-#
-#cd integration_tests
-#chmod u+x prepare_splunk.sh
-#echo $(green "Preparing Splunk instance")
-#./prepare_splunk.sh
-#sed -i "s/###SPLUNK_TOKEN###/$(cat hec_token)/" values.yaml
-#sed -i "s/###LOAD_BALANCER_ID###/$(hostname -I | cut -d " " -f1)/" values.yaml
-#sudo docker run -d -p 161:161/udp tandrup/snmpsim
-#
-#sudo microk8s enable helm3 storage dns rbac openebs
-#sudo systemctl enable iscsid
-#yes $(hostname -I | cut -d " " -f1)/32 | sudo microk8s enable metallb
-#
+
+sudo docker pull splunk/splunk:latest
+echo $(green "Running Splunk in Docker")
+sudo docker run -d -p 8000:8000 -p 8088:8088 -p 8089:8089 -e SPLUNK_START_ARGS='--accept-license' -e SPLUNK_PASSWORD='changeme2' splunk/splunk:latest
+
+wait_for_splunk
+
+cd integration_tests
+chmod u+x prepare_splunk.sh
+echo $(green "Preparing Splunk instance")
+./prepare_splunk.sh
+sed -i "s/###SPLUNK_TOKEN###/$(cat hec_token)/" values.yaml
+sed -i "s/###LOAD_BALANCER_ID###/$(hostname -I | cut -d " " -f1)/" values.yaml
+sudo docker run -d -p 161:161/udp tandrup/snmpsim
+
+sudo microk8s enable helm3 storage dns rbac openebs
+sudo systemctl enable iscsid
+yes $(hostname -I | cut -d " " -f1)/32 | sudo microk8s enable metallb
+
 #cd ~/splunk-connect-for-snmp/charts/splunk-connect-for-snmp
 #microk8s helm3 dep update
 #cd ~/splunk-connect-for-snmp/integration_tests
