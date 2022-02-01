bbd24bf5b6ecde19723acf76d5a1325f61601c47
uoboda-splunk <uoboda@splunk.com>
2022-02-01 23:47:19 +0100

    try with s3 upload

--- integration_tests/scripts/result_analyzer.py
+++ integration_tests/scripts/result_analyzer.py
@@ -8,3 +8,5 @@ for suite in xml:
     assert suite.errors == 0, "Some errors detected in test results"
     assert suite.failures == 0, "Some failures detected in test results"
     assert suite.skipped == 0, "Some tests were skipped"
+
+print("All tests PASSED")
